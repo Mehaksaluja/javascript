130779ed99bb321a5161d6c3360185584108fa36
Mehaksaluja <salujamehak11@gmail.com>
2024-02-10 15:21:24 +0530

    convertion of datatypes

---
+++ basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score =true;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 3;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 => true , 0 => false
+// "" =>false
+// mehak =>error
+
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber);
+
