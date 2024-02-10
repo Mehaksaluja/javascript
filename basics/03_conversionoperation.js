003ec6df810a8d147cbeaf29dc4e0fc48aa16d5c
Mehaksaluja <salujamehak11@gmail.com>
2024-02-10 15:46:56 +0530

    operations and comparison

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -24,3 +24,31 @@ let someNumber = 33;
 let StringNumber = String(someNumber);
 console.log(typeof StringNumber);
 
+// ****************operations*****************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+3);
+// console.log(2*3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "mehak";
+let str2 = "saluja";
+let str3 = str1+ str2;
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+console.log(+true);
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2+2;
+let gameCounter= 100;
+gameCounter++;
+console.log(gameCounter);
